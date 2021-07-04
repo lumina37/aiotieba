40a00cf0e92587a8e21d5c142d612e7a29ce6e45
ZhiliaoBackend <starry.qvq@gmail.com>
2021-07-04 11:57:40 +0800

    Update __init__.py

--- tiebaBrowser/__init__.py
+++ tiebaBrowser/__init__.py
@@ -2,19 +2,18 @@
 """
 @Author: Starry
 @License: MIT
-@Homepage: https://github.com/Starry-OvO
+@Homepage: https://github.com/Starry-OvO/Tieba-Cloud-Review
 @Required Modules:
-    sudo pip install mysql-connector
-    sudo pip install lxml
-    sudo pip install bs4
-    sudo pip install pillow
-    sudo pip install imagehash
-    sudo pip install pypinyin
+    pip install pymysql
+    pip install lxml
+    pip install bs4
+    pip install pillow
+    pip install imagehash
 
     可能需要的第三方yum源: Raven(https://centos.pkgs.org/8/raven-x86_64/raven-release-1.0-1.el8.noarch.rpm.html)
     使用 [rpm -Uvh xxx.rpm] 来安装Raven源
     用 [sudo yum install zbar-devel] 来安装zbar支持
-    用 [sudo pip install pyzbar] 来安装pyzbar
+    用 [pip install pyzbar] 来安装pyzbar
 """
 
 from .data_structure import *
