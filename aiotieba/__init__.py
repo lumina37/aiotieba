e2db8fa3d3d922e28b07fcb9a23687d2481e463a
Starry-OvO <starry.qvq@gmail.com>
2022-05-29 23:56:21 +0800

    Update 2.7.2_beta

--- aiotieba/__init__.py
+++ aiotieba/__init__.py
@@ -1,16 +1,14 @@
 # -*- coding:utf-8 -*-
 """
-@Version 2.7.2_beta
+@Version: 2.7.2_beta
 @Author: starry.qvq@gmail.com
 @License: Unlicense
 @Homepage: https://github.com/Starry-OvO/Tieba-Manager
 @Required Python Version: 3.8+
-@Required Modules: pyyaml aiohttp[speedups] protobuf lxml beautifulsoup4 pycryptodome aiomysql opencv-contrib-python
+@Required Modules: pyyaml aiohttp protobuf lxml beautifulsoup4 pycryptodome aiomysql opencv-contrib-python
 """
 
-import asyncio
 import signal
-import sys
 
 from .api import *
 from .logger import *
@@ -25,6 +23,3 @@ def terminate(signal_number, frame):
 
 
 signal.signal(signal.SIGTERM, terminate)
-
-if sys.platform == "win32" and sys.version_info.minor >= 8:
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
