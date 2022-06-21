857e5bbeaec63a61478eba5e2b321c635797ca08
Starry-OvO <starry.qvq@gmail.com>
2022-06-21 23:11:10 +0800

    Update 2.7.5_beta

--- .github/test/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# prepare test script
-mv $HOME/.github/test/test_all.py $HOME
-# prepare config
-mkdir $HOME/config
-gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output $HOME/config/config.yaml $HOME/.github/test/config.yaml.gpg
