cf482f4a6455b26597f86ad170da0151797a3c96
Starry-OvO <starry.qvq@gmail.com>
2022-08-29 19:26:28 +0800

    Update 2.9.2a0

--- aiotieba/reviewer.py
+++ aiotieba/reviewer.py
@@ -843,7 +843,9 @@ class Reviewer(_ReviewUtils):
 
         if post.reply_num > 10:
             last_comments = await self.get_comments(post.tid, post.pid, pn=post.reply_num // 30 + 1)
-            return post.comments + last_comments.objs
+            comments = set(last_comments)
+            comments.update(post.comments)
+            return comments
 
         else:
             return post.comments
@@ -1062,7 +1064,9 @@ class Reviewer(_ReviewUtils):
 
         if post.reply_num > 10:
             last_comments = await self.get_comments(post.tid, post.pid, pn=post.reply_num // 30 + 1)
-            return post.comments + last_comments.objs
+            comments = set(last_comments)
+            comments.update(post.comments)
+            return comments
 
         else:
             return post.comments
