ee67aff2fd15d3d8737b6475a524ea33c46567a5
Starry-OvO <starry.qvq@gmail.com>
2022-12-26 21:52:18 +0800

    Update 2.10.0a0

--- aiotieba/client.py
+++ aiotieba/client.py
@@ -278,7 +278,7 @@ class Client(object):
 
     _use_env_proxy = False
 
-    latest_version: ClassVar[str] = "12.34.1.0"  # 这是目前的最新版本
+    latest_version: ClassVar[str] = "12.34.3.0"  # 这是目前的最新版本
     # no_fold_version: ClassVar[str] = "12.12.1.0"  # 这是最后一个回复列表不发生折叠的版本
     post_version: ClassVar[str] = "9.1.0.0"  # 发帖使用极速版
 
@@ -643,7 +643,7 @@ class Client(object):
             aiohttp.MultipartWriter: 只可用于贴吧客户端
         """
 
-        writer = aiohttp.MultipartWriter('form-data', boundary=f"*-672328094-42-{random.randint(0,9)}")
+        writer = aiohttp.MultipartWriter('form-data', boundary=f"*-672328094--{random.randint(0,9)}")
         payload_headers = {
             aiohttp.hdrs.CONTENT_DISPOSITION: aiohttp.helpers.content_disposition_header(
                 'form-data', name='data', filename='file'
