5f87c0e5a8c238f3423c9ec90687f7e09340870c
Starry-OvO <starry.qvq@gmail.com>
2023-02-11 07:58:03 +0800

    refactor: less include path

--- setup.py
+++ setup.py
@@ -8,19 +8,21 @@ ext_src_in_strs = [str(f) for f in ext_path.glob('*.c')]
 
 ext_3rdparty_include_dirs = [
     third_party_path,
-    third_party_path / "mbedtls/library",
     third_party_path / "mbedtls/include",
+]
+ext_3rdparty_include_in_strs = [str(d) for d in ext_3rdparty_include_dirs]
+ext_3rdparty_src_dirs = [
+    third_party_path,
+    third_party_path / "mbedtls/library",
     third_party_path / "base32",
     third_party_path / "crc",
     third_party_path / "xxHash",
-    third_party_path / "rapidjson/internal",
 ]
-ext_3rdparty_include_in_strs = [str(d) for d in ext_3rdparty_include_dirs]
 
 
 def _yield_file() -> str:
-    for include_dir in ext_3rdparty_include_dirs:
-        for f in include_dir.glob('*.c'):
+    for src_dir in ext_3rdparty_src_dirs:
+        for f in src_dir.glob('*.c'):
             yield str(f)
 
 
