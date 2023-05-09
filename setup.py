d572f1aff5a2809b4d19c48296993c830703ecd8
Starry-OvO <starry.qvq@gmail.com>
2023-05-09 17:11:00 +0800

    refactor: rename 3rdparty to thirdparty

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 from setuptools import Extension, setup
 
-third_party_path = Path("3rdparty")
+third_party_path = Path("thirdparty")
 ext_path = Path("aiotieba/helper/crypto")
 ext_src_in_strs = [str(f) for f in ext_path.glob('*.c')]
 
-ext_3rdparty_include_dirs = [
+ext_thirdparty_include_dirs = [
     third_party_path,
     third_party_path / "mbedtls/include",
 ]
-ext_3rdparty_include_in_strs = [str(d) for d in ext_3rdparty_include_dirs]
-ext_3rdparty_src_dirs = [
+ext_thirdparty_include_in_strs = [str(d) for d in ext_thirdparty_include_dirs]
+ext_thirdparty_src_dirs = [
     third_party_path,
     third_party_path / "mbedtls/library",
     third_party_path / "base32",
@@ -21,17 +21,17 @@ ext_3rdparty_src_dirs = [
 
 
 def _yield_file() -> str:
-    for src_dir in ext_3rdparty_src_dirs:
+    for src_dir in ext_thirdparty_src_dirs:
         for f in src_dir.glob('*.c'):
             yield str(f)
 
 
-ext_3rdparty_src_in_strs = list(_yield_file())
+ext_thirdparty_src_in_strs = list(_yield_file())
 
 ext_crypto_module = Extension(
     "aiotieba.helper.crypto.crypto",
-    sources=ext_src_in_strs + ext_3rdparty_src_in_strs,
-    include_dirs=ext_3rdparty_include_in_strs,
+    sources=ext_src_in_strs + ext_thirdparty_src_in_strs,
+    include_dirs=ext_thirdparty_include_in_strs,
     define_macros=[('XXH_INLINE_ALL', None)],
     language='c',
 )
