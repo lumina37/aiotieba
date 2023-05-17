9f997a0cfc7893308327ac88122adb3741c8aa6e
Starry-OvO <starry.qvq@gmail.com>
2023-05-17 17:33:28 +0800

    chore: rerun failed tests

--- tests/test_get_comments.py
+++ tests/test_get_comments.py
@@ -3,6 +3,7 @@ import pytest
 import aiotieba as tb
 
 
+@pytest.mark.flaky(reruns=3, reruns_delay=2.0)
 @pytest.mark.asyncio
 async def test_Comments(client: tb.Client):
     comments = await client.get_comments(8211419000, 146544112004)
@@ -148,7 +149,7 @@ async def test_Comments(client: tb.Client):
     comment = comments[1]
     assert comment.reply_to_id != 0
 
-
+@pytest.mark.flaky(reruns=3, reruns_delay=2.0)
 @pytest.mark.asyncio
 async def test_FragLink(client: tb.Client):
     comments = await client.get_comments(8211419000, 146546137439)
