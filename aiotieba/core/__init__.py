750800f9937421deb33b50f8e3af43c46cd0f679
Starry-OvO <starry.qvq@gmail.com>
2023-06-22 19:15:07 +0800

    fix: missing import of `TypeWebsocketCallback`

--- aiotieba/core/__init__.py
+++ aiotieba/core/__init__.py
@@ -1,4 +1,4 @@
 from .account import Account
 from .http import HttpCore
 from .net import NetCore, TimeConfig
-from .websocket import WsCore, WsResponse
+from .websocket import TypeWebsocketCallback, WsCore, WsResponse
