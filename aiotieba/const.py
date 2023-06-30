190965aabd8a3feb8c9235d661c430f73a8ffda6
Starry-OvO <starry.qvq@gmail.com>
2023-06-30 18:35:18 +0800

    fix: `CHECK_URL_PERFIX`

--- aiotieba/const.py
+++ aiotieba/const.py
@@ -7,4 +7,4 @@ APP_INSECURE_SCHEME = "http"
 APP_BASE_HOST = "tiebac.baidu.com"
 WEB_BASE_HOST = "tieba.baidu.com"
 
-CHECK_URL_PERFIX = "http://tieba.baidu.com/mo/q/checkurl?url="
+CHECK_URL_PERFIX = "https://tieba.baidu.com/mo/q/checkurl?url="
