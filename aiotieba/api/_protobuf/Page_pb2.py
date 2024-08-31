aaf23da33d33e332eea0f4c326d2e90dc13a7379
Starry-OvO <starry.qvq@gmail.com>
2024-08-31 18:48:38 +0800

    feat: remove runtime checker in protobuf

--- aiotieba/api/_protobuf/Page_pb2.py
+++ aiotieba/api/_protobuf/Page_pb2.py
@@ -2,11 +2,8 @@
 
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
-_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 27, 0, '', 'Page.proto')
 
 _sym_db = _symbol_database.Default()
 
--- scripts/proto_compile.py
+++ scripts/proto_compile.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import subprocess
+from collections.abc import Iterator
 from pathlib import Path
 
 commom_proto_pth = Path("aiotieba/api/_protobuf")
@@ -8,18 +11,37 @@ for fpth in commom_proto_pth.glob('*_pb2.py'):
 
 subprocess.run("protoc --python_out=. *.proto", cwd=str(commom_proto_pth), check=True, timeout=60.0)
 
+
+def row_filter(rows: list[str], import_perfix: str) -> Iterator[str]:
+    is_runtime_checker = False
+    for row in rows:
+        if row.startswith('#'):
+            continue
+
+        if not is_runtime_checker and row.startswith("_runtime"):
+            is_runtime_checker = True
+        if is_runtime_checker and row.startswith(')'):
+            is_runtime_checker = False
+            continue
+        if is_runtime_checker:
+            continue
+
+        if "import runtime_version" in row:
+            continue
+
+        if row.startswith("import"):
+            row = import_perfix + row
+
+        yield row
+
+
 for fpth in commom_proto_pth.glob('*_pb2.py'):
     bak_fpth = fpth.with_suffix('.bak')
     with (
         fpth.open('r') as f,
         bak_fpth.open('w') as bak_f,
     ):
-        for row in f.readlines():
-            if row.startswith('#'):
-                continue
-            if row.startswith('import'):
-                row = "from . " + row
-            bak_f.write(row)
+        bak_f.writelines(row_filter(f, "from . "))
     fpth.unlink()
     bak_fpth.rename(fpth)
 
@@ -35,12 +57,7 @@ for mod_pth in Path("aiotieba/api").glob('*/protobuf'):
             fpth.open('r') as f,
             bak_fpth.open('w') as bak_f,
         ):
-            for row in f.readlines():
-                if row.startswith('#'):
-                    continue
-                if row.startswith('import'):
-                    row = "from ..._protobuf " + row
-                bak_f.write(row)
+            bak_f.writelines(row_filter(f, "from ..._protobuf "))
         fpth.unlink()
         bak_fpth.rename(fpth)
 
