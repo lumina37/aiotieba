40bd2df7491e4d58877609fb607e7f951af49966
lumina37 <starry.qvq@gmail.com>
2026-04-16 04:38:09 +0800

    fix: remove unused import

--- src/aiotieba/helper/crypto/__init__.py
+++ src/aiotieba/helper/crypto/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import hashlib
-
 from .const import APP_SALT, MISC_SALT, PC_SALT
 from .crypto import c3_aid, cuid_galaxy2, enuid, rc4_42
 from .sign import compute_sign, sign
